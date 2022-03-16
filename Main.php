ead8350d93dbe303cc835c9ec069f4ff9e748493
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-16 19:52:50 +0300

    Test

---
+++ Main.php
@@ -0,0 +1,22 @@
+<?php
+
+require "input_array.php";
+
+function is_leap_year($year) {
+    if ($year % 4 != 0) {
+        return false;
+    }
+    elseif ($year % 100 == 0) {
+        if ($year % 400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return true;
+    }
+    
+    echo "test";
+}
