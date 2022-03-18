6e1818a5261ec039877bf0fb24ee4591eac8f282
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-03-18 12:31:29 +0300

    require

--- Main.php
+++ Main.php
@@ -1,6 +1,53 @@
 <?php
 
-require_once "input_array.php";
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+
+//include 'input_array.php';
 
 function getPartsFromFullname($surname, $name, $patronymic) {
     return $surname.' '.$name.' '.$patronymic;
@@ -16,5 +63,5 @@ function getPartsShortName($surname, $name, $patronymic) {
     return $fullname;}
 
 echo getPartsFromFullname('t', 'y', 'z');
-$example_persons_array =;
-echo $example_persons_array[0][0];
+
+echo $example_persons_array[0]['fullname'];
